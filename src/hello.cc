46a2d60950cc93bd8c2985db5f69b6b6186be36a
Michał Kubik <bopke2@gmail.com>
2023-09-11 01:02:35 +0200

    Maketh code bri'ish compatible bruv

--- src/hello.cc
+++ src/hello.cc
@@ -5,10 +5,10 @@ Napi::String Method(const Napi::CallbackInfo& info) {
     return Napi::String::New(env, "world");
 }
 
-Napi::Object Init(Napi::Env env, Napi::Object exports) {
+Napi::Object Innit(Napi::Env env, Napi::Object exports) {
     exports.Set(Napi::String::New(env, "hello"),
                 Napi::Function::New(env, Method));
     return exports;
 }
 
-NODE_API_MODULE(hello, Init)
+NODE_API_MODULE(hello, Innit)
